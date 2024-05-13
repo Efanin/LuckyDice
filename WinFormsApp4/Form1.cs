8f8f240b59dc10a63829593403da04aec84379a8
Efanin <efanin.igor@gmail.com>
2024-05-13 15:51:12 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp4/Form1.cs
@@ -0,0 +1,52 @@
+namespace WinFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        Game game = new();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            game.Bet(1, button1, label1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            game.Bet(2, button2, label1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            game.Bet(3, button3, label1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            game.Bet(4, button4, label1);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            game.Bet(5, button5, label1);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            game.Bet(6, button6, label1);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            game.Start(label1, pictureBox1, pictureBox2, pictureBox3);
+            game.Clear(button1, button2, button3, button4, button5, button6);
+        }
+    }
+}
---
+++ WinFormsApp4/Game.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp4
+{
+    internal class Game
+    {
+        private int money = 50;
+        private int[] bets = new int[6];
+        private int[] win = new int[3];
+        public void Bet(int index, Button button, Label label)
+        {
+            bets[index-1]++;
+            money--;
+            button.Text = bets[index-1].ToString();
+            label.Text = money.ToString();
+        }
+        public void Start(Label label,params PictureBox[] picture)
+        {
+            for(int i=0;i<win.Length;i++)
+            {
+                win[i] = new Random().Next(6);
+                picture[i].Image = Dice(win[i]);
+            }
+            for (int i = 0; i < win.Length; i++)
+            {
+                money += bets[win[i]] * 2;
+            }
+            label.Text = money.ToString();
+        }
+        public void Clear(params Button[] button)
+        {
+            for(int i=0;i< button.Length;i++)
+            {
+                button[i].Text = "0";
+                bets[i] = 0;
+            }
+        }
+        private Bitmap Dice(int index)
+        {
+            return index switch
+            {
+                0 => Resource1._1,
+                1 => Resource1._2,
+                2 => Resource1._3,
+                3 => Resource1._4,
+                4 => Resource1._5,
+                5 => Resource1._6
+            };
+        }
+    }
+}
